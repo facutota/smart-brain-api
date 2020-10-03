17067427943e9af798d3d6096e7d0d0c9f9d1338
Facundo Medina <Facutota@yahoo.com.ar>
2020-10-03 02:17:50 -0300

    change proc

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const db = knex({
 
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -39,6 +40,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(process.env.PORT || 3000 , ()=> {
-  console.log('app is running on port '${process.env.PORT}'');
+app.listen(PORT || 3000 , ()=> {
+  console.log('app is running on port ${PORT}');
 })
